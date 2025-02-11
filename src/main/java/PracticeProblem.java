4bb89600a28c0106104904daffc49a74877aa4bd
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-02-11 15:05:58 +0000

    Update Pracitce

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,26 @@ public class PracticeProblem {
 
 	}
 
+	public static int sum (int num1, int num2){
+		int ad;
+		ad = num1 + num2;
+		return ad;
+	}
+
+	public static int difference (int num3, int num4){
+		int sub;
+		sub = num3 - num4;
+		return sub;
+	}
+
+	public static double product (double num5, double num6){
+		double prdct;
+		prdct = num5*num6;
+		return prdct;
+	}
+
+	public static String removefirst(String str1){
+		String less;
+	}
+
 }
