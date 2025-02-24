14fe2433814e48f0d93c41c2c2bd151d0d039fde
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-02-24 03:44:20 +0000

    Update Practice

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,25 +5,43 @@ public class PracticeProblem {
 	}
 
 	public static int sum (int num1, int num2){
-		int ad;
-		ad = num1 + num2;
-		return ad;
+		int sum = num1 + num2;
+		return sum;
 	}
 
 	public static int difference (int num3, int num4){
-		int sub;
-		sub = num3 - num4;
+		int sub = num3 - num4;
 		return sub;
 	}
 
 	public static double product (double num5, double num6){
-		double prdct;
-		prdct = num5*num6;
-		return prdct;
+		double product = num5*num6;
+		return product;
 	}
 
-	public static String removefirst(String str1){
-		String less;
+	public static String removeFirst(String str){
+		return str.substring(1);
 	}
+
+	public static int combinedLength(String a, String b){
+		return a.length()+b.length();
+	}
+
+	public static boolean isEven(int even){
+		return even % 2 == 0;
+	}
+
+	public static boolean isOdd(int odd){
+		return odd % 2 != 0;
+	}
+
+	public static boolean isPositive(int postive){
+		return positive > 0;
+	}
+
+	public static boolean isNegative(int negative){
+		return negative < 0;
+	}
+
 
 }
